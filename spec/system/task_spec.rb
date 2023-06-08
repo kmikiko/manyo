47fdfd6221d1dc29a038905a8874a73a7feeab8c
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-08 17:54:28 +0900

    Add Sytem spec test

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) {FactoryBot.create(:task, name: 'aaa')}
-    # task2 = FactoryBot.create(:task, name: 'bbb')
-    # task3 = FactoryBot.create(:task, name: 'ccc')
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'task[name]', with: 'aaaaaa'
-        fill_in 'task[detail]', with: 'bbb'
+        fill_in 'task[name]', with: '洗濯'
+        fill_in 'task[detail]', with: 'シーツを洗う'
         click_on "登録"
-        expect(page).to have_content 'aaaaaa'
-        expect(page).to have_content 'bbb'
+        expect(page).to have_content '洗濯'
+        expect(page).to have_content 'シーツを洗う'
       end
     end
   end
   describe '一覧表示機能' do
     before do
-      task1 = FactoryBot.create(:task, name: 'bbb')
-      task2 = FactoryBot.create(:task, name: 'ccc')
+      @task1 = FactoryBot.create(:task, name: 'bbb')
+      @task2 = FactoryBot.create(:task, name: 'ccc')
       visit tasks_path
     end
     context '一覧画面に遷移した場合' do
@@ -29,12 +27,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        task1 = FactoryBot.create(:task, name: 'bbb')
-        task2 = FactoryBot.create(:task, name: 'ccc')
-        expect(page.text).to match(/#{task2.name}[\s\S]*#{task1.name}/)
+        expect(page.text).to match(/#{@task1.name}[\s\S]*#{task.name}/)
         # task_lists = all('.task_row')
-        # expect(task_lists[0]).to eq 'ccc'
-        # expect(task_lists[1]).to eq 'bbb'
+        # expect(task_lists[0]).to have_content 'ccc'
+        # expect(task_lists[1]).to have_content 'bbb'
       end
     end
   end
