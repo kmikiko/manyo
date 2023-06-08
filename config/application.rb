2c8c79af5920f43057e5f8a544c460b477b64f03
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-08 13:44:48 +0900

    Apply i18n

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module TaskManagementApp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework :rspec,
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) {FactoryBot.create(:task, name: 'aaa')}
+    # task2 = FactoryBot.create(:task, name: 'bbb')
+    # task3 = FactoryBot.create(:task, name: 'ccc')
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
         fill_in 'task[name]', with: 'aaaaaa'
-        fill_in 'task[detail]', with: 'bbbbb'
+        fill_in 'task[detail]', with: 'bbb'
         click_on "登録"
         expect(page).to have_content 'aaaaaa'
-        expect(page).to have_content 'bbbbb'
+        expect(page).to have_content 'bbb'
       end
     end
   end
   describe '一覧表示機能' do
+    before do
+      task1 = FactoryBot.create(:task, name: 'bbb')
+      task2 = FactoryBot.create(:task, name: 'ccc')
+      visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'aaa')
-        task2 = FactoryBot.create(:task, name: 'bbb')
-        visit tasks_path
-        expect(page).to have_content task.name
-        expect(page).to have_content task2.name
+        expect(page).to have_content 'bbb'
+        expect(page).to have_content 'ccc'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task1 = FactoryBot.create(:task, name: 'bbb')
+        task2 = FactoryBot.create(:task, name: 'ccc')
+        expect(page.text).to match(/#{task2.name}[\s\S]*#{task1.name}/)
+        # task_lists = all('.task_row')
+        # expect(task_lists[0]).to eq 'ccc'
+        # expect(task_lists[1]).to eq 'bbb'
       end
     end
   end
