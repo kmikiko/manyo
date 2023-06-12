17466e6800b9bdbaa9f84a56ba2adedceae7ba32
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-13 08:26:41 +0900

    Create User#new html

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
