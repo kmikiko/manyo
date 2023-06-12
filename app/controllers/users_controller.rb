81d580404592e61c251b8566fe985df025937aa9
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-13 08:45:17 +0900

    Create login function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -6,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      redirect_to user_path(@user.id)
     else
       render :new
     end
@@ -21,4 +23,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-  end
+    end
+end
